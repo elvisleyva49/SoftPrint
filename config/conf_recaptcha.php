0f0701fcf97acc9fbff42cc559cfb11815ef76ef
Andree Flores Melendez <af2017057494@virtual.upt.pe>
2024-12-10 16:05:02 -0500

    Correccion v3

--- config/conf_recaptcha.php
+++ config/conf_recaptcha.php
@@ -2,7 +2,7 @@
   require_once '../vendor/autoload.php';
 
   $clientID = '682277659949-921nbukvt3i42if1pv267cb4htb0nop4.apps.googleusercontent.com';
-  $clientSecret = '6Ldh3pUqAAAAANmVCBm4ywqE4x4hwaZ_7NHZzfkE';
+  $clientSecret = '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';
   $redirectUri = 'http://localhost/PRUEBA/hola.php';
 
   $client = new Google_Client();
